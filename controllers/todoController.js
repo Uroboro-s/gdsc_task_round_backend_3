afec01f1a8bf5b455d9c55ee18474505e9155738
Janmeyjai Mishra <mishrajanmeyjai100@gmail.com>
2023-12-23 23:59:41 +0530

    Completed all routes

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -50,12 +50,15 @@ exports.createOne = asyncHandler( async (req, res, next) => {
 
 
 //UPDATE one todo specified by id
-exports.updateOne = asyncHandler( async (req, res, next) => {
-    const todo = await Todo.findById(req.params.id);
+exports.updateOne = asyncHandler( async (req, res, next) => {  
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
 
     if(!todo)
         return next(new Error("404 not found?"));
-    console.log("here");
+    //console.log("here");
     res.status(200).json({
         data: todo,
     });
@@ -65,5 +68,11 @@ exports.updateOne = asyncHandler( async (req, res, next) => {
 
 //DELETE one todo specified using id
 exports.deleteOne = asyncHandler( async (req, res, next) => {
-    res.send("yoho");
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+
+    if(!todo)
+        return next(new Error("404 not found?"));
+
+    res.status(200);
+    res.json();
 });
